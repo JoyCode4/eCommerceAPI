f57f611c810b78e0e3e180808e8bd15320458d32
JoyCode4 <joywadhonkar4@gmail.com>
2023-11-13 11:31:35 +0530

    1.created router for the products 2.created contollers for the GET request getAll products. 3.created the model of the products with dummy data.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
+import ProductRouter from "./src/features/product/product.routes.js";
 const port = 8000;
 const server = express();
 
-server.get("/", (req, res) => {
-  res.status(200).json("Welcome to the server");
-});
+server.use("/api/products", ProductRouter);
 
 server.listen(port, () => {
   console.log("server listening on port: " + port);
---
+++ src/features/product/product.controller.js
@@ -0,0 +1,14 @@
+import ProductModel from "./product.model.js";
+
+export default class ProductController {
+  getAllProducts(req, res) {
+    const products = ProductModel.getAll();
+    res.status(200).send(products);
+  }
+
+  addProduct(req, res) {}
+
+  rateProduct(req, res) {}
+
+  getOneProduct(req, res) {}
+}
