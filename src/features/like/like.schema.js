214e65cddbe09e7a5a1ccb83531cf661f6a39f6a
JoyCode4 <joywadhonkar4@gmail.com>
2023-12-18 00:40:06 +0530

    trying the mongoose middleware such as document middleware and query middleware

--- src/features/like/like.schema.js
+++ src/features/like/like.schema.js
@@ -13,4 +13,24 @@ export const likeSchema = new mongoose.Schema({
     type: String,
     enum: ["Product", "Category"],
   },
-});
+})
+  // Document Middleware : Pre middleware of mongoose before save the doc
+  .pre("save", (next) => {
+    console.log("Document Saving is ongoing...");
+    next();
+  })
+  // Document Middleware : Post middleware of mongoose after save the doc
+  .post("save", (docs) => {
+    console.log("Document is saved...");
+    console.log(docs);
+  })
+  // Query Middleware : Pre middleware of mongoose before find function of mongoose
+  .pre("find", (next) => {
+    console.log("finding ...");
+    next();
+  })
+  // Query Middleware : Post middleware of mongoose after find function of mongoose
+  .post("find", (docs) => {
+    console.log("docs is been found...");
+    console.log(docs);
+  });
